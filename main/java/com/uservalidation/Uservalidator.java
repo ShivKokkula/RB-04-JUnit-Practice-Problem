068a87ba717e4a4411140228267fdd4c060c1ab0
ShivKokkula <shivbk987@gmail.com>
2021-05-29 16:51:41 +0530

    Refactor - added last name validation in UC-2-Last-Name-Validation branch

--- main/java/com/uservalidation/Uservalidator.java
+++ main/java/com/uservalidation/Uservalidator.java
@@ -3,9 +3,14 @@ package com.uservalidation;
 import java.util.regex.Pattern;
 public class Uservalidator {
     private static final String First_Name_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String Last_Name_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 
     public boolean validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(First_Name_PATTERN);
         return pattern.matcher(fname).matches();
+    }
+    public boolean validateLastName(String lname) {
+        Pattern pattern = Pattern.compile(Last_Name_PATTERN);
+        return pattern.matcher(lname).matches();
     }
 }
--- test/java/com/uservalidation/UserValidatorTest.java
+++ test/java/com/uservalidation/UserValidatorTest.java
@@ -11,4 +11,10 @@ public class UserValidatorTest {
         boolean result = uservalidator.validateFirstName("Shivasagar");
         Assertions.assertEquals(true, result);
     }
+    @Test
+    void givenLastName_WhenProper_ShouldReturnTrue() {
+        Uservalidator uservalidator = new Uservalidator();
+        boolean result = uservalidator.validateLastName("Kokkula");
+        Assertions.assertEquals(true,result);
+    }
 }
